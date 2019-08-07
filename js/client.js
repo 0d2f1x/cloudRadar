06d6a85a94a0ad98cc5c7f50238539724a0c5cb0
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-08-07 07:19:52 +0400

    Patch

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("ws://xorox.herokuapp.com:3000");
+let ws = new WebSocket("ws://xorox.herokuapp.com");
     ws.onmessage = ({data}) => {
       if (data != "Connected!") {
       unpackData(data);
