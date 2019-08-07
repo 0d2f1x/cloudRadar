c741a480b1baec637696b0e771206bfb7efd16b2
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-08-07 11:09:24 +0400

    icons

--- js/drawer.js
+++ js/drawer.js
@@ -1,10 +1,10 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var size = 10;
-icon();
+icon("X");
 
 function unpackData(data){
-    icon();
+    icon((Math.floor(Math.random() * 10) + 1));
     document.getElementById("map").src = "maps/de_dust2_radar.png";
     data = JSON.parse(data);
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
@@ -30,10 +30,10 @@ function drawRectTeam(X, Y){
     ctx.drawImage(img, X-11, Y-7, 22, 14);  x 22/2 and y 14/2 */
 }
 
-function icon() {
+function icon(name) {
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/png';
     link.rel = 'icon';
-    link.href = "images/favicons/"+ (Math.floor(Math.random() * 10) + 1) + ".png";
+    link.href = "images/favicons/"+ name + ".png";
     document.getElementsByTagName('head')[0].appendChild(link);
 }
