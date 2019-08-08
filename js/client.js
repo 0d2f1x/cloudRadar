6a315f22992f52d7852211cb55626fdb384508f6
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-08-08 19:19:28 +0400

    added maps and somethin like this

--- js/client.js
+++ js/client.js
@@ -2,6 +2,8 @@ let ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
     ws.onmessage = ({data}) => {
       if (data != "Connected!") {
       unpackData(data);
+      } else {
+        ws.send('Connected!');
       }
     }
     ws.onopen = function(evt) { document.title = "Xorox's maps | 🌐"; }; 
