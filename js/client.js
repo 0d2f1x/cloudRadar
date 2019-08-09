ac89536118d8fd9fc55b5c4643d8bb95b8f7e685
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-08-09 14:57:39 +0400

    on close connection change to default dance

--- js/client.js
+++ js/client.js
@@ -1,4 +1,7 @@
 let ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
     ws.onmessage = ({data}) => { if (data != "Connected!") unpackData(data) }
     ws.onopen = function(evt) { document.title = "Xorox's maps | 🌐"; }; 
-    ws.onclose = function(evt) { document.title = "Xorox's maps | ❌"; };
+    ws.onclose = function(evt) {
+       document.title = "Xorox's maps | ❌"; 
+       document.getElementById("map").src = "imgaes/default_dance.png";
+      };
