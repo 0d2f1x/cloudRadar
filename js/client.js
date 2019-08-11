c7583542e165f37e1c601dfd875db8a3539a375f
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-08-11 12:38:44 +0400

    getback

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 let ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
-    ws.onmessage = ({data}) => { if (data != "Connected!" && data != "ping!") unpackData(data) }
+    ws.onmessage = ({data}) => { if (data != "Connected!") unpackData(data) }
     ws.onopen = function(evt) { document.title = "Xorox's maps | 🌐"; }; 
     ws.onclose = function(evt) {
        document.title = "Xorox's maps | ❌"; 
