8484575ce4475c1b99347dbecfe6385f61a13432
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-08-12 15:22:30 +0400

    color fix

--- js/drawer.js
+++ js/drawer.js
@@ -9,8 +9,12 @@ function unpackData(data) {
         document.getElementById("map").src = "maps/"+ map +"_radar.png";
         ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
         if (data[map].Ct.player0 != undefined) { ctColor = "blue"; tColor = "red"; } else { ctColor = "red"; tColor = "blue"; }
-        for (var i in data[map].Ct) if (data[map].Ct[i].Health > 0) drawRect(data[map].Ct[i].X, data[map].Ct[i].Y, ctColor);
-        for (var i in data[map].T) if (data[map].T[i].Health > 0) drawRect(data[map].T[i].X, data[map].T[i].Y, tColor);
+        for (var i in data[map].Ct) if (data[map].Ct[i].Health > 0 && data[map].Ct[i].Dormant == false) {
+            drawRect(data[map].Ct[i].X, data[map].Ct[i].Y, ctColor);
+        }
+        for (var i in data[map].T) if (data[map].T[i].Health > 0 && data[map].T[i].Dormant == false) {
+            drawRect(data[map].T[i].X, data[map].T[i].Y, tColor);
+        }
     } catch (exc) { 
         console.log("Incorrect input");
         document.getElementById("map").src = "images/default_dance.png";
