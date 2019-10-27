06a46bbf87aeff247aeb51fa9cb65c2577cde4ac
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-10-27 23:58:34 +0400

    added maps

--- js/drawer.js
+++ js/drawer.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var borderColor = "black"
+var borderColor = "black";
+var dangerZonceColor = "#39FF14";
 var size = 10;
 var output;
 
@@ -11,13 +12,16 @@ function unpackData(data) {
         var map = Object.keys(data)[0];
         document.getElementById("map").src = "maps/"+ map +"_radar.png";
         ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-        if (data[map].Ct.player0 != undefined) { ctColor = "#66FFFF"; tColor = "red"; } else { ctColor = "red"; tColor = "#66FFFF"; }
         if (map != "dz_blacksite" || map != "dz_sirocco") {
+            if (data[map].Ct.player0 != undefined) { ctColor = "#66FFFF"; tColor = "red"; } else { ctColor = "red"; tColor = "#66FFFF"; }
             for (var i in data[map].Ct) {
                 if (data[map].Ct[i].Health > 0 && data[map].Ct[i].Dormant == false) 
                     drawRect(data[map].Ct[i].X, data[map].Ct[i].Y, ctColor);
             }
-        }        
+        } else{
+            ctColor = dangerZonceColor;
+            tColor = dangerZonceColor;
+        }    
         for (var i in data[map].T) {
             if (data[map].T[i].Health > 0 && data[map].T[i].Dormant == false) 
                 drawRect(data[map].T[i].X, data[map].T[i].Y, tColor);
