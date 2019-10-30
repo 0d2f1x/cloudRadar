718db2779716b75d4017302f12afb6685278468f
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-10-30 19:50:30 +0400

    routes.js added, handlebars

---
+++ routes.js
@@ -0,0 +1,62 @@
+module.exports = function(app){
+
+app.get('/js/drawer.js', function(req, res){
+    res.sendFile(__dirname + '/js/drawer.js');
+  });
+  
+  app.get('/js/client.js', function(req, res){
+    res.sendFile(__dirname + '/js/client.js');
+  });
+  
+  app.get('/styles.css', function(req, res){
+    res.sendFile(__dirname + '/styles.css');
+  });
+  
+  app.get('/images/background.png', function(req, res){
+    res.sendFile(__dirname + '/images/background.png');
+  });
+  
+  app.get('/images/default_dance.png', function(req, res){
+    res.sendFile(__dirname + '/images/default_dance.png');
+  });
+  
+  app.get('/maps/de_dust2_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/de_dust2_radar.png');
+  });
+  
+  app.get('/maps/de_mirage_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/de_mirage_radar.png');
+  });
+  
+  app.get('/maps/de_inferno_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/de_inferno_radar.png');
+  });
+  
+  app.get('/maps/de_cache_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/de_cache_radar.png');
+  });
+  
+  app.get('/maps/de_overpass_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/de_overpass_radar.png');
+  });
+  
+  app.get('/maps/de_train_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/de_train_radar.png');
+  });
+  
+  app.get('/maps/dz_blacksite_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/dz_blacksite_radar.png');
+  });
+  
+  app.get('/maps/dz_sirocco_radar.png', function(req, res){
+    res.sendFile(__dirname + '/maps/dz_sirocco_radar.png');
+  });
+  
+  app.get('/images/hideo.jpg', function(req, res){
+    res.sendFile(__dirname + '/images/hideo.jpg');
+  });
+  
+  app.get('/images/favicons/X.png', function(req, res){
+    res.sendFile(__dirname + '/images/favicons/X.png');
+  });
+}
--- server.js
+++ server.js
@@ -1,83 +1,21 @@
 const WebSocket = require('ws');
+const path = require('path');
+const http = require('http');
 const express = require('express');
-const path = require('path')
-const http = require('http')
-
 const app = express();
-const httpServer = http.createServer(app);
 
+const httpServer = http.createServer(app);
 var port = process.env.PORT || 80;
 
-
+require('./routes')(app);
 app.set('views',path.join(__dirname,'/pages/'));
 app.set("view engine","hbs");
 
-{
-app.get('/js/drawer.js', function(req, res){
-  res.sendFile(__dirname + '/js/drawer.js');
-});
-
-app.get('/js/client.js', function(req, res){
-  res.sendFile(__dirname + '/js/client.js');
-});
-
-app.get('/styles.css', function(req, res){
-  res.sendFile(__dirname + '/styles.css');
-});
-
-app.get('/images/background.png', function(req, res){
-  res.sendFile(__dirname + '/images/background.png');
-});
-
-app.get('/images/default_dance.png', function(req, res){
-  res.sendFile(__dirname + '/images/default_dance.png');
-});
-
-app.get('/maps/de_dust2_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/de_dust2_radar.png');
-});
-
-app.get('/maps/de_mirage_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/de_mirage_radar.png');
-});
-
-app.get('/maps/de_inferno_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/de_inferno_radar.png');
-});
-
-app.get('/maps/de_cache_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/de_cache_radar.png');
-});
-
-app.get('/maps/de_overpass_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/de_overpass_radar.png');
-});
-
-app.get('/maps/de_train_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/de_train_radar.png');
-});
-
-app.get('/maps/dz_blacksite_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/dz_blacksite_radar.png');
-});
-
-app.get('/maps/dz_sirocco_radar.png', function(req, res){
-  res.sendFile(__dirname + '/maps/dz_sirocco_radar.png');
-});
-
-app.get('/images/hideo.jpg', function(req, res){
-  res.sendFile(__dirname + '/images/hideo.jpg');
-});
-
-app.get('/images/favicons/X.png', function(req, res){
-  res.sendFile(__dirname + '/images/favicons/X.png');
-});
-}
 
 app.get('*', function(req, res){
-  //res.sendFile(__dirname + '/pages/maps.hbs');
-  res.render('maps', {lol:123});
+  res.render('maps');
 });
+
 
 const server = new WebSocket.Server({
   'server': httpServer
