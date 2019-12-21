6de97ce2f94560e7cb2c419ca62e150cda2c44fd
Roman <36329005+Xeroxlaser@users.noreply.github.com>
2019-12-21 12:47:54 +0400

    fix sleep

--- server.js
+++ server.js
@@ -27,3 +27,27 @@ server.on("connection", ws => {
   });
 });
 httpServer.listen(port);
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'xorox.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    //console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    //console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
